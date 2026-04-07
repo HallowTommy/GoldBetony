d1030ece67ffc6e4b2614cf0b9c0f38ec34879e9
Hallow_Tommy <turtlehaze13@gmail.com>
2026-04-07 17:37:18 +0300

    update project

--- GoldBetony/ContentView.swift
+++ GoldBetony/ContentView.swift
@@ -2,15 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var store = SportsStore.shared
+    @AppStorage("hasSeenOnboarding") private var hasSeenOnboarding = false
 
     var body: some View {
-        WebGateRouter {
-            FacadeTabView()
-                .environmentObject(store)
-                .task { await store.loadIfNeeded() }
-        } webContent: { url in
-            WebGateView(url: url)
-                .ignoresSafeArea()
+        if hasSeenOnboarding {
+            WebGateRouter {
+                FacadeTabView()
+                    .environmentObject(store)
+                    .task { await store.loadIfNeeded() }
+            } webContent: { url in
+                WebGateView(url: url)
+                    .ignoresSafeArea()
+            }
+        } else {
+            OnboardingView()
+                .transition(.opacity)
         }
     }
 }
